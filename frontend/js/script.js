96c6e3af4c9bae4e06f60aa192c321f1f6b0ccfc
MW-Limah <cdrmw16@gmail.com>
2024-07-19 12:09:21 -0400

    Update script.js

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -83,7 +83,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://webchat-backend-888k.onrender.com")
     websocket.onmessage = processMessage
 }
 
